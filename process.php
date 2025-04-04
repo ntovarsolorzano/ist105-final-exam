b4a006d2c9d8514b1c53aae1a6490197e1b240f5
Nelson Tovar <ntovar@fiec.espol.edu.ec>
2025-04-04 12:35:56 -0700

    v2 Corrected the process.php

--- process.php
+++ process.php
@@ -5,7 +5,7 @@ if (!isset($_POST['items'])) {
 }
 
 $selected = $_POST['items'];
-$arg_string = implode(",", array_map('intval', $selected)); // Ensures only numbers
+$arg_string = implode(",", array_map('intval', $selected)); // Ensures only numbers.
 
 $escaped_arg = escapeshellarg($arg_string);
 $output = shell_exec("python3 party_planner.py $escaped_arg 2>&1");
